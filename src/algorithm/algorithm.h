d1324328241540544f3e77193917d56bc975cad1
Mario <48070830+mjcarranza@users.noreply.github.com>
2021-10-31 15:04:14 -0600

    Update algorithm.h

--- src/algorithm/algorithm.h
+++ src/algorithm/algorithm.h
@@ -1,8 +1,41 @@
-#ifndef LZ77_TOOL_ALGORITHM_H
-#define LZ77_TOOL_ALGORITHM_H
+#ifndef LZ77_ALGORITHMLZ77_H
+#define LZ77_ALGORITHMLZ77_H
 
-class Algorithm {
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
 
+class algorithmLZ77 {
+    // atributos
+    int const window = 13;
+    int const dataBuffer = 6;
+    int const searchBuffer = window - dataBuffer;
+    int offset;
+    int match;
+    int current = 0;
+    int fila=0;
+    string nextChar ;
+    string word = "cabracadabrarrarrad";
+    string **compresMat = new string *[word.length()];
+
+    string dBArr[6];
+    string sBArr[7];
+
+
+public:
+    // constructor
+    algorithmLZ77();
+
+    // métodos
+    void compress();
+    void inputChars();
+    void inputSearchBuffer();
+    bool checkMatch();
+    void setNext();
+    void solution();
+    void createMatrix();
+    void showMatrix();
 };
 
-#endif //LZ77_TOOL_ALGORITHM_H
+
+#endif //LZ77_ALGORITHMLZ77_H
